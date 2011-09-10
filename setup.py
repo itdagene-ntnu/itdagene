c8df63c01ece2f0a745295ebdde52c2bd0d423bd
Rolf <relekang@gmail.com>
2011-09-10 08:49:32 +0200

    Created buildout environment

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "itdagene",
+    version = "1.0",
+    url = 'http://itdagene.no',
+    license = 'Proprietary',
+    description = "itDAGENE",
+    author = 'Rolf Erik Lekang',
+    requires = [],
+    install_requires = ['setuptools'],
+)
+
+
