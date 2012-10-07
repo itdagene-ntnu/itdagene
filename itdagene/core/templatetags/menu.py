b9ad847626ebb04282eacfa0f449bf73589f837b
Rolf Erik Lekang <relekang@gmail.com>
2012-10-08 00:18:08 +0200

    fixes urls in public menu

--- itdagene/core/templatetags/menu.py
+++ itdagene/core/templatetags/menu.py
@@ -1,4 +1,3 @@
-# encoding: utf-8
 from django.template import Library
 from django.core.urlresolvers import reverse
 from django.conf import settings
@@ -10,12 +9,12 @@ register = Library()
 def menu(request):
     menu = [
             {'title': _('Home'), 'url': reverse('frontpage')},
-#            {'title': _('What we offer'), 'url': reverse('app.pages.views.view_page', args=['what-we-offer'])},
-            {'title': _('Program'), 'url': reverse('app.events.views.public_event_list')},
-            {'title': _('About itDAGENE'), 'url': reverse('app.pages.views.view_page', args=['om-oss'])},
-            {'title': _('Joblistings'), 'url': reverse('app.career.views.joblistings.list_joblistings')},
-            {'title': _('Board'), 'url': reverse('core.profiles.views.public_profiles')},
-            {'title': _('Attending companies'), 'url': reverse('app.pages.views.view_page', args=['deltakende_bedrifter'])},
+#            {'title': _('What we offer'), 'url': reverse('itdagene.app.pages.views.view_page', args=['what-we-offer'])},
+            {'title': _('Program'), 'url': reverse('itdagene.app.events.views.public_event_list')},
+            {'title': _('About itDAGENE'), 'url': reverse('itdagene.app.pages.views.view_page', args=['om-oss'])},
+            {'title': _('Joblistings'), 'url': reverse('itdagene.app.career.views.joblistings.list_joblistings')},
+            {'title': _('Board'), 'url': reverse('itdagene.core.profiles.views.public_profiles')},
+            {'title': _('Attending companies'), 'url': reverse('itdagene.app.pages.views.view_page', args=['deltakende_bedrifter'])},
 #            {'title': _('Login'), 'url': '/login/?next=/'},
     ]
     return {'menu': menu, 'request': request}
