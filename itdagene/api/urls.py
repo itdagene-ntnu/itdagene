3a0c93bb7675b32b4a56a9570986d7bbf1db3e69
Rolf Erik Lekang <relekang@gmail.com>
2012-11-11 16:27:58 +0100

    add list to worker api

--- itdagene/api/urls.py
+++ itdagene/api/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns('itdagene.api.views',
     url(r'^users/$', 'users.get'),
     url(r'^worker/(?P<username>\w+)/$', 'workschedule.get'),
+    url(r'^workers/$', 'workschedule.list'),
 
 )
--- itdagene/api/views/workschedule.py
+++ itdagene/api/views/workschedule.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
+from django.http import Http404
 from django.utils.translation import ugettext_lazy as _
 from itdagene.api.util import render_json, login_or_token_required
 from itdagene.app.workschedule.models import Worker
@@ -15,3 +16,23 @@ def get(request, username):
 
     except (Worker.DoesNotExist):
         return render_json(error='Could not find worker')
+
+def list(request):
+
+    if not request.GET.get('token') == 'jtQu6iPH3WhQLH':
+        raise Http404
+
+    worker_list = []
+
+    for worker in Worker.objects.all():
+        has_met = True
+        for schedule in worker.in_schedules.all():
+            if not schedule.has_met:
+                has_met = False
+        worker_list.append({
+            'username': worker.username,
+            'name': worker.name,
+            'has_met': has_met
+        })
+
+    return render_json(worker_list)
