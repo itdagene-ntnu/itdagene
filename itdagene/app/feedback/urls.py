05184bc09f8c6a6beebdf3bc084e0be29fea17bd
Rolf Erik Lekang <relekang@gmail.com>
2012-11-12 12:00:39 +0100

    add report for feedback

--- itdagene/app/feedback/urls.py
+++ itdagene/app/feedback/urls.py
@@ -4,6 +4,7 @@ from django.http import HttpResponsePermanentRedirect
 urlpatterns = patterns('itdagene.app.feedback.views',
     url(r'^$', lambda r: HttpResponsePermanentRedirect('/feedback/issues/')),
     url(r'^report/$', 'evalutions.report'),
+    url(r'^report/(?P<year>\d{4})/$', 'evalutions.report'),
 )
 
 urlpatterns += patterns('itdagene.app.feedback.views.issues',
--- itdagene/app/feedback/views/evalutions.py
+++ itdagene/app/feedback/views/evalutions.py
@@ -36,10 +36,29 @@ def handle_evaluation(request, hash):
     })
 
 @login_required
-def report (request):
-    preferences = Preference.current_preference()
+def report (request, year=None):
+    if year is None:
+        preferences = Preference.current_preference()
+    else:
+        preferences = get_object_or_404(Preference, year=year)
     evaluations = Evaluation.objects.filter(hash__preference=preferences)
-    return render(request, 'feedback/evaluations/report.html',{
-        'evaluations': evaluations,
-        'preferences': preferences
-    })
+
+    if evaluations.count():
+        if evaluations.exclude(internship_marathon_rating=0).count():
+            avg_internship_marathon_rating = sum([e.internship_marathon_rating for e in evaluations])/evaluations.exclude(internship_marathon_rating=0).count()
+
+        if evaluations.exclude(course_rating=0).count():
+            avg_course_rating = sum([e.course_rating for e in evaluations])/evaluations.exclude(course_rating=0).count()
+
+        if evaluations.exclude(visitors_rating=0).count():
+            avg_visitors_rating = sum([e.visitors_rating for e in evaluations])/evaluations.count()
+
+        if evaluations.exclude(interview_location_rating=0).count():
+            avg_interview_location_rating = sum([e.interview_location_rating for e in evaluations])/evaluations.exclude(interview_location_rating=0).count()
+
+        if evaluations.exclude(banquet_rating=0).count():
+            avg_banquet_rating = sum([e.banquet_rating for e in evaluations])/evaluations.exclude(banquet_rating=0).count()
+
+        percentage_want_to_come_back = (evaluations.filter(want_to_come_back=True).count()/evaluations.count())*100
+
+    return render(request, 'feedback/evaluations/report.html', locals())
