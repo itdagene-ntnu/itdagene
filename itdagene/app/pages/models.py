bf8e6086a664fd78d2af7abf41b947637351e2eb
Adrian Alexander Eriksen <adrian@aeriksen.com>
2013-10-31 22:07:03 +0100

    Død kode fjernet og PEP8

--- itdagene/app/pages/models.py
+++ itdagene/app/pages/models.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+
 from django.conf import settings
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 from itdagene.core.log.models import LogItem
 from itdagene.core.models import BaseModel
-from django.utils.translation import ugettext_lazy as _
-from itdagene.core.util import expire_page_cache
+
 
 class Page(BaseModel):
     title = models.CharField(max_length=100, verbose_name=_('title'))
@@ -23,9 +25,7 @@ class Page(BaseModel):
         verbose_name_plural = _('Pages')
 
     def save(self, *args, **kwargs):
-        if self.pk: action = 'EDIT'
-        else: action = 'CREATE'
+        action = 'EDIT' if self.pk else 'CREATE'
         super(Page, self).save(*args, **kwargs)
         LogItem.log_it(self, action, 1)
         self.last_saved = datetime.now()
-#        expire_page_cache('view_page', args=[self.slug])
--- itdagene/app/pages/urls.py
+++ itdagene/app/pages/urls.py
@@ -1,11 +1,35 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('itdagene.app.pages.views',
-    (r'^$', 'view_page'),
-    url(r'^pages/$', 'admin', name='pages'),
-    (r'^pages/add$', 'add'),
-    (r'^(?P<lang_code>[a-z][a-z])/(?P<slug>.*)/edit/$', 'edit'),
-    (r'^(?P<lang_code>[a-z][a-z])/(?P<slug>.*)/$', 'view_page'),
-    (r'^(?P<slug>.*)/edit/$', 'edit'),
-    (r'^(?P<slug>.*)/$', 'view_page'),
+    url(
+        regex=r'^$',
+        view='view_page'
+    ),
+    url(
+        regex=r'^pages/$',
+        view='admin',
+        name='pages'
+    ),
+    url(
+        regex=r'^pages/add$',
+        view='add'
+    ),
+    url(
+        regex=r'^(?P<lang_code>[a-z][a-z])/(?P<slug>.*)/edit/$',
+        view='edit'
+    ),
+    url(
+        regex=r'^(?P<lang_code>[a-z][a-z])/(?P<slug>.*)/$',
+        view='view_page'
+    ),
+    url(
+        regex=r'^(?P<slug>.*)/edit/$',
+        view='edit'
+    ),
+    url(
+        regex=r'^(?P<slug>.*)/$',
+        view='view_page'
+
+
+    ),
 )
