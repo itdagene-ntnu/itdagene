f56893a796fbcc40285805ff8694bf6d471f3218
Adrian Alexander Eriksen <adrian@aeriksen.com>
2013-11-02 19:32:21 +0100

    Fixed bug in backend views

--- itdagene/app/pages/urls.py
+++ itdagene/app/pages/urls.py
@@ -2,6 +2,15 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('itdagene.app.pages.views',
     url(
+        regex=r'^pages/$',
+        view='admin',
+        name='pages'
+    ),
+    url(
+        regex=r'^pages/add/$',
+        view='add'
+    ),
+    url(
         regex=r'^$',
         view='view_page'
     ),
@@ -20,14 +29,5 @@ urlpatterns = patterns('itdagene.app.pages.views',
     url(
         regex=r'^(?P<lang_code>[a-z][a-z])/(?P<slug>[-_\w]+)/edit/$',
         view='edit'
-    ),
-    url(
-        regex=r'^pages/$',
-        view='admin',
-        name='pages'
-    ),
-    url(
-        regex=r'^pages/add$',
-        view='add'
     ),
 )
