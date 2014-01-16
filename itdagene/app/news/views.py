e4be2b25ce426375fcfe0b14f54cc49d2b927a73
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-01-16 22:08:41 +0100

    automatic updating of year on frontpage template, removed caching from public newsitems, as it would sometimes render the admin template view of it for non-authenticated users

--- itdagene/app/news/views.py
+++ itdagene/app/news/views.py
@@ -6,18 +6,19 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 
-@cache_page
-def view_announcement (request, id):
+
+def view_announcement(request, id):
     announcement = get_object_or_404(Announcement, pk=id)
     return render(request,'news/view.html',{'announcement': announcement})
 
+
 @permission_required('news.change_announcement')
-def create_announcement (request):
+def create_announcement(request):
     return edit_announcement(request)
 
 
 @permission_required('news.change_announcement')
-def edit_announcement (request, id=False):
+def edit_announcement(request, id=False):
     if id:
         ann = get_object_or_404(Announcement, pk=id)
         form = AnnouncementForm(instance=ann)
