ee86401e5a6bc327d78f0270ad96211ffbb49fb6
Adrian Alexander Eriksen <adrian@aeriksen.com>
2014-03-01 15:27:27 +0100

    Bugfix

--- itdagene/core/profiles/tests.py
+++ itdagene/core/profiles/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 
 class ModelTest(TestCase):
-    def test_that_new_useres_alo_have_a_profile(self):
+    def test_that_new_users_also_have_a_profile(self):
         user = User.objects.create_user('adrian', 'adrian@example.com')
 
         self.assertEqual(user.id, user.profile.user.id)
