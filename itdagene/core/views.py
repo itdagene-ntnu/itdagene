de47e1568447c56fae5bc418e192797f2de8311e
Adrian Alexander Eriksen <adrian@aeriksen.com>
2014-03-29 23:25:37 +0100

    Fjerner tidligere profiladministrasjon

--- itdagene/core/views.py
+++ itdagene/core/views.py
@@ -4,7 +4,6 @@ from django.utils.translation import ugettext_lazy as _
 from django.template.context import RequestContext
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 from itdagene.core.auth import generate_password
-from itdagene.core.profiles.forms import ForgotPasswordForm
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
@@ -17,24 +16,6 @@ def permission_denied (request, args=None):
     View that displays permission denied and info about the permission that is required
     """
     return render(request,'core/permission_denied.html', args)
-
-def forgot_password (request):
-    form = ForgotPasswordForm()
-    if request.method == 'POST':
-        form = ForgotPasswordForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            try:
-                user = User.objects.get(email=email)
-                password = generate_password()
-                user.set_password(password)
-                user.save()
-                send_mail(_('Your password for itDAGENE'),_('Your new password is: ' + password),settings.FROM_ADDRESS,(user.email,))
-                return redirect(reverse('itdagene.core.profiles.views.me'))
-            except(TypeError, User.DoesNotExist):
-                pass
-
-    return render(request,'forgot_password.html',{'form':form})
 
 @csrf_exempt
 def backup_for_remote (request):
