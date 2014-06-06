801c3cb5f853cd3f19bfeeaa0b7b0b771d76c316
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-06-06 22:37:57 +0200

    start of admin utilities

---
+++ itdagene/api/views/admin/cache.py
@@ -0,0 +1,8 @@
+from itdagene.api.util import login_or_token_required
+from itdagene.core.util.cache import flush_cache
+from django.http import HttpResponse
+
+@login_or_token_required()
+def flush():
+    flush_cache()
+    return HttpResponse()
