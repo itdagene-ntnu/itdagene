7d8e0442d350a115317dfcd3549c441cfec50289
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-06-07 01:42:52 +0200

    cache flushing implemented

--- itdagene/api/views/admin/cache.py
+++ itdagene/api/views/admin/cache.py
@@ -3,6 +3,6 @@ from itdagene.core.util.cache import flush_cache
 from django.http import HttpResponse
 
 @login_or_token_required()
-def flush():
+def flush(request):
     flush_cache()
     return HttpResponse()
---
+++ itdagene/app/admin/static/admin/js/dashboard.js
@@ -0,0 +1,18 @@
+var flashMessage = function(message, type) {
+    $('#page-content-wrapper').prepend("<div class=\"alert alert-" + type + "\">" +
+        "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">&times;</button>" + message +
+        "</div>");
+};
+
+$(document).ready(function() {
+    $('#flush-cache').click(function() {
+        if (confirm('Are you sure you want to flush the cache?')) {
+            $.ajax({
+                type: 'GET',
+                url: '/api/admin/cache/flush'
+            }).success(function(data) {
+                flashMessage("Cache cleared", "success");
+            });
+        }
+    });
+});
