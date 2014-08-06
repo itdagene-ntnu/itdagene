4bea76c7264e6a4d246a9e65c4b900bab8b4e4e6
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-08-06 22:31:14 +0200

    update gitignore, new fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,33 +1,24 @@
-import os
-import sys
 
 from fabric.decorators import task
 from fabric.state import env
-
 from django_fabric import App
 
-
-# This is to make fabric able to read our django settings
-sys.path.append(os.path.dirname(__file__))
-
 env.user = 'itdagene'
-env.hosts = ['itdagene.no']
+env.hosts = ['itdagene.abakus.no']
 
 site = App(
     project_paths={
-        'prod': '/home/itdagene/itdagene/',
+        'production': '/home/itdagene/itdagene'
+    },
+    urls={
+        'production': 'http://itdagene.no'
+    },
+    restart_command={
+        'production': 'sudo service uwsgi restart'
     },
     project_package='itdagene',
-    restart_command='touch /etc/uwsgi/apps-enabled/itdagene.ini',
-    requirements={
-        'prod': 'requirements-prod.txt'
-    }
+    
 )
 
-
-@task
-def deploy():
-    return site.deploy_prod()
-
-clone_prod_data = task(site.clone_prod_data)
+deploy = task(site.deploy)
 test = task(site.test)
