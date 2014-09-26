f13a7268cd13e3c3b0a8165e6aa8f94b452b90d5
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2014-09-26 19:35:55 +0200

    Add box design admin app

--- itdagene/app/users/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from django.core.urlresolvers import reverse
-
-
-class UsersViewsTests(TestCase):
-    def test_user_list_url_returns_right_view(self):
-        response = self.client.get(reverse('users:list'))
-        self.assertEqual(response.status_code, 200)
--- itdagene/core/profiles/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from itdagene.core.models import User
-
-
-class ModelTest(TestCase):
-    def test_that_new_users_also_have_a_profile(self):
-        user = User.objects.create_user('adrian', 'adrian@example.com')
-
-        self.assertEqual(user.id, user.profile.user.id)
