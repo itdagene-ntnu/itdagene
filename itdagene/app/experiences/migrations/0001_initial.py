13215826620a33b4f04f3d43641dfcb8c611c938
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2014-10-07 17:30:19 +0200

    New migrations Experience

---
+++ itdagene/app/experiences/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0015_preference_active'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Experience',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date_created', models.DateTimeField(editable=False)),
+                ('date_saved', models.DateTimeField(editable=False)),
+                ('position', models.PositiveIntegerField(default=8, verbose_name='Position', choices=[(0, b'Leder'), (1, b'Nestleder'), (2, b'Okonomi'), (3, b'Bedriftskontaktansvarlig'), (4, b'Logistikk'), (5, b'Markedsforing'), (6, b'Web'), (7, b'Bankett'), (8, b'Bedriftskontakt')])),
+                ('last_updated', models.DateField(auto_now=True)),
+                ('text', models.TextField(null=True, verbose_name='Experiences', blank=True)),
+                ('creator', models.ForeignKey(related_name=b'experience_creator', editable=False, to=settings.AUTH_USER_MODEL)),
+                ('saved_by', models.ForeignKey(related_name=b'experience_saved_by', editable=False, to=settings.AUTH_USER_MODEL)),
+                ('year', models.ForeignKey(verbose_name='Preference', to='core.Preference')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+    ]
