f9cbd61984c26fd3762351fbb0aab6e36ef814fd
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-01-21 17:06:57 +0100

    Add frontend base code

---
+++ itdagene/assets/frontend/js/index.js
@@ -0,0 +1,23 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var moment = require('moment');
+var request = require('superagent');
+var react_bootstrap = require('react-bootstrap');
+
+
+var App = React.createClass({
+
+    render: function(){
+
+        return (
+            <div></div>
+            );
+    }
+
+});
+
+React.render(
+  <App />,
+  document.getElementById('app')
+);
