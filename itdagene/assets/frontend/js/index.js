6635aecdaf4bd8aa00d335d62e24fc2ed8987fdf
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-01-30 16:17:16 +0100

    Add page footer with instagram and twitter

--- itdagene/assets/frontend/js/index.js
+++ itdagene/assets/frontend/js/index.js
@@ -4,20 +4,3 @@ var React = require('react');
 var moment = require('moment');
 var request = require('superagent');
 var react_bootstrap = require('react-bootstrap');
-
-
-var App = React.createClass({
-
-    render: function(){
-
-        return (
-            <div></div>
-            );
-    }
-
-});
-
-React.render(
-  <App />,
-  document.getElementById('app')
-);
--- itdagene/core/context_processors.py
+++ itdagene/core/context_processors.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from itdagene.core.models import Preference
+from itdagene.core.twitter import get_latest_tweet
+from itdagene.core.instagram_helper import get_tag_images
 
 
 def site_processor(request):
@@ -20,3 +22,20 @@ def utils_processor(request):
     context['preferences'] = Preference.current_preference()
 
     return context
+
+
+def twitter(request):
+    """
+    Get the latest tweet as text.
+    """
+    context = {}
+    context['tweet'] = get_latest_tweet()
+
+    return context
+
+
+def instagram(request):
+    """
+    Return a list og the 9 latest instagram images
+    """
+    return {'instagram': get_tag_images()}
