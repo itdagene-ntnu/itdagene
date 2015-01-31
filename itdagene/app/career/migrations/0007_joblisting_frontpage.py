29a5fc733ccdad8a56220cc04dc03b3abc3a285c
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-01-31 20:34:35 +0100

    Added a new row on the home page with announcements, joblistings and a quiz panel. The quiz panel is not ready.

---
+++ itdagene/app/career/migrations/0007_joblisting_frontpage.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('career', '0006_auto_20141121_2054'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='joblisting',
+            name='frontpage',
+            field=models.BooleanField(default=False, verbose_name='Frontpage'),
+            preserve_default=True,
+        ),
+    ]
