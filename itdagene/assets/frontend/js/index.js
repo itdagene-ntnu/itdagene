83cfd10abb5cdfa065b72b1da8ac81508d8c3b36
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-02-01 17:01:01 +0100

    added company overview

--- itdagene/assets/frontend/js/index.js
+++ itdagene/assets/frontend/js/index.js
@@ -3,12 +3,23 @@
 var React = require('react');
 
 $(document).ready(function() {
+
     $(window).scroll(function () {
-        console.log($(".navbar").offset().top);
         if ($(".navbar").offset().top > 50) {
             $(".navbar-fixed-top").addClass("top-nav-collapse");
         } else {
             $(".navbar-fixed-top").removeClass("top-nav-collapse");
         }
     });
+
+    var companiesContainer = $('.companies-holder');
+    if (companiesContainer.length == 1) {
+        var buttons = $('.btn-days');
+        buttons.click(function(e) {
+            var self = $(this);
+            buttons.removeClass('active');
+            self.addClass('active');
+        });
+    }
+
 });
