e266ec628e54fc2328417775e1338b07656eafb6
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-02-01 17:55:17 +0100

    Lookup companies in the database

--- itdagene/core/notifications/middleware.py
+++ itdagene/core/notifications/middleware.py
@@ -6,7 +6,7 @@ class NotificationsMiddleware(object):
     def process_request(self, request):
         if request.user.is_authenticated():
             path = request.path
-            notifications = Notification.objects.filter(user=request.user, read=False)
+            notifications = Notification.objects.filter(user=request.user, read=False).select_related('content_object')
             if notifications:
                 for notification in notifications:
                     if notification.content_object.get_absolute_url() == path:
