f801684e761cd4ca434a82080650ddcb933bf040
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-04-23 18:05:10 +0200

    Merge migrations

---
+++ itdagene/app/company/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('company', '0007_auto_20150423_0019'),
+        ('company', '0007_auto_20150423_0137'),
+    ]
+
+    operations = [
+    ]
