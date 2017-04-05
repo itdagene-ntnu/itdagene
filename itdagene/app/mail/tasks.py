87d4f18a67f6523c1a014952129839d0d51be5c0
Orhan Hirsch <orhanhenrik@gmail.com>
2017-04-05 11:39:59 +0200

    add celery 4

--- itdagene/app/mail/tasks.py
+++ itdagene/app/mail/tasks.py
@@ -1,22 +1,21 @@
 # -*- coding: utf8 -*-
 
-from celery import task
-
+from itdagene.celery import app
 from .senders import meeting_send_invite as msi
 from .senders import notifications_send_email as nse
 from .senders import send_comment_email as ce
 
 
-@task()
+@app.task()
 def meeting_send_invite(users, meeting):
     msi(users, meeting)
 
 
-@task()
+@app.task()
 def send_notification_message(notification):
     nse(notification)
 
 
-@task()
+@app.task()
 def send_comment_email(comment):
     ce(comment)
---
+++ itdagene/celery.py
@@ -0,0 +1,31 @@
+import os
+
+import celery
+from django.conf import settings
+
+class Celery(celery.Celery):
+    def on_configure(self):
+        pass
+
+        import raven
+        from raven.contrib.celery import register_signal, register_logger_signal
+
+        client = raven.Client()
+
+        # register a custom filter to filter out duplicate logs
+        register_logger_signal(client)
+
+        # hook into the Celery error handler
+        register_signal(client)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'itdagene.settings')
+
+app = Celery('itdagene')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
