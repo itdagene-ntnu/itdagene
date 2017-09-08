a8a1bf5bdf47013772a50b4cb7402fdaf00e19ea
Orhan Hirsch <orhanhenrik@gmail.com>
2017-09-08 12:06:58 +0200

    fix missing migration

---
+++ itdagene/core/migrations/0023_auto_20170908_1206.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0022_auto_20160215_1141'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='user',
+            name='year',
+            field=models.PositiveIntegerField(default=2017, max_length=3000, blank=True, help_text='Year the user was active.', null=True, verbose_name='Active Year'),
+            preserve_default=True,
+        ),
+    ]
