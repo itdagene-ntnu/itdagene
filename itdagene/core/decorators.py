5c240192125cf7bcfd52c9b5c154ae4f8ceea5b4
Aleksander <aleksander.vestlund@hotmail.com>
2024-02-15 23:09:16 +0100

    Refactor decorators, context processors, views, auth, and middleware

--- itdagene/core/decorators.py
+++ itdagene/core/decorators.py
@@ -1,14 +1,16 @@
+from typing import Any, Optional
+
 from django.contrib.auth.decorators import user_passes_test
 
 
-def superuser_required(login_url=None, raise_exception=False):
+def superuser_required(login_url: Optional[str] = None, raise_exception: Any = False):
     def check_perms(user):
         return user.is_superuser
 
     return user_passes_test(check_perms, login_url=login_url)
 
 
-def staff_required(login_url=None, raise_exception=False):
+def staff_required(login_url: Optional[str] = None, raise_exception: Any = False):
     def check_perms(user):
         return user.is_staff
 
--- itdagene/core/middleware.py
+++ itdagene/core/middleware.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from django.http import HttpRequest
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.deprecation import MiddlewareMixin
@@ -7,38 +10,37 @@ from itdagene.core.models import Preference
 
 
 class ForceDefaultLanguageMiddleware(MiddlewareMixin):
+    """Ignore Accept-Language HTTP headers.
+
+    This will force the I18N machinery to always choose
+    settings.LANGUAGE_CODE as the default initial language, unless
+    another one is set via sessions or cookies.
+
+    Should be installed `before` any middleware that checks
+    request.META['HTTP_ACCEPT_LANGUAGE'], namely
+    django.middleware.locale.LocaleMiddleware.
     """
-    Ignore Accept-Language HTTP headers
 
-    This will force the I18N machinery to always choose settings.LANGUAGE_CODE
-    as the default initial language, unless another one is set via sessions or cookies
-
-    Should be installed *before* any middleware that checks request.META['HTTP_ACCEPT_LANGUAGE'],
-    namely django.middleware.locale.LocaleMiddleware
-    """
-
-    def process_request(self, request):
-        if "HTTP_ACCEPT_LANGUAGE" in request.META:
-            del request.META["HTTP_ACCEPT_LANGUAGE"]
+    def process_request(self, request: HttpRequest) -> None:
+        http_accept_language = "HTTP_ACCEPT_LANGUAGE"
+        if http_accept_language in request.META:
+            del request.META[http_accept_language]
 
 
 class UnderDevelopmentMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        if (
-            request.path == reverse("itdagene.under_development")
-            or "login" in request.path
-        ):
+    def process_request(self, request: HttpRequest) -> Optional[HttpResponseRedirect]:
+        if request.path == reverse("itdagene.under_development"):
             return
-        development = Preference.current_preference().development_mode
-        if development:
-            if request.user.is_authenticated:
-                if not request.user.is_staff:
-                    return HttpResponseRedirect(reverse("itdagene.under_development"))
-            else:
-                return HttpResponseRedirect(reverse("itdagene.under_development"))
+        if "login" in request.path:
+            return
+        if not Preference.current_preference().development_mode:
+            return
+        if request.user.is_authenticated and request.user.is_staff:
+            return
+        return HttpResponseRedirect(reverse("itdagene.under_development"))
 
 
 class CurrentUserMiddleware(MiddlewareMixin):
-    def process_request(self, request):
+    def process_request(self, request: HttpRequest) -> None:
         user = getattr(request, "user", None)
         set_current_user_function(lambda: user)
