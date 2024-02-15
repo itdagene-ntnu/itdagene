5bf9b64e9f503b33023d2be673dd01ee2c8c1d79
Aleksander <aleksander.vestlund@hotmail.com>
2024-02-15 23:18:50 +0100

    Annotate decorators

--- itdagene/core/decorators.py
+++ itdagene/core/decorators.py
@@ -4,14 +4,14 @@ from django.contrib.auth.decorators import user_passes_test
 
 
 def superuser_required(login_url: Optional[str] = None, raise_exception: Any = False):
-    def check_perms(user):
+    def check_perms(user) -> bool:
         return user.is_superuser
 
-    return user_passes_test(check_perms, login_url=login_url)
+    return user_passes_test(check_perms, login_url)
 
 
 def staff_required(login_url: Optional[str] = None, raise_exception: Any = False):
-    def check_perms(user):
+    def check_perms(user) -> bool:
         return user.is_staff
 
-    return user_passes_test(check_perms, login_url=login_url)
+    return user_passes_test(check_perms, login_url)
