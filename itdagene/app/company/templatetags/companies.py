7be7a26e4ebbf9aec9d60ecace58e91905152072
overmannen <jesper231@gmail.com>
2025-04-26 16:39:19 +0200

    merge package forms into one form

--- itdagene/app/company/templatetags/companies.py
+++ itdagene/app/company/templatetags/companies.py
@@ -1,28 +1,14 @@
 from django.template import Library
 
 from itdagene.app.company.forms import (
-    BookCompanyForm,
-    CompanyStatusForm,
-    WaitingListCompanyForm,
+    CompanyPackageForm,
 )
 
 
 register = Library()
 
 
-@register.inclusion_tag("company/templatetags/book_form.html")
-def book_form(company) -> dict:
-    form = BookCompanyForm(instance=company)
-    return {"form": form}
-
-
-@register.inclusion_tag("company/templatetags/waiting_list_form.html")
-def waiting_list_form(company) -> dict:
-    form = WaitingListCompanyForm(instance=company)
-    return {"form": form}
-
-
-@register.inclusion_tag("company/templatetags/status_form.html")
-def status_form(company) -> dict:
-    form = CompanyStatusForm(instance=company)
+@register.inclusion_tag("company/templatetags/package_form.html")
+def package_form(company) -> dict:
+    form = CompanyPackageForm(instance=company)
     return {"form": form}
