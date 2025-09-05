63662ed4d3ed50aadca0a1c73c606274a8ee7d20
christiangryt <dodif380@gmail.com>
2025-09-05 10:50:05 +0200

    resolved merge conflict by recompiling messages

--- itdagene/graphql/filters.py
+++ itdagene/graphql/filters.py
@@ -4,16 +4,16 @@ from itdagene.app.career.models import Joblisting as ItdageneJoblisting
 
 
 class JoblistingFilter(FilterSet):
-    from_year = NumberFilter(field_name="to_year", lookup_expr="gte")
-    to_year = NumberFilter(field_name="from_year", lookup_expr="lte")
+    from_grade = NumberFilter(field_name="to_grade", lookup_expr="gte")
+    to_grade = NumberFilter(field_name="from_grade", lookup_expr="lte")
 
     class Meta:
         model = ItdageneJoblisting
         fields = [
             "type",
             "company",
-            "to_year",
-            "from_year",
+            "to_grade",
+            "from_grade",
             "towns",
             "is_summerjob_marathon",
         ]
